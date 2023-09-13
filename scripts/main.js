fe1a80d6120c9766385913fa20cbc1c78f4dddc1
zjjblack <2290329807@qq.com>
2023-09-13 23:29:16 +0800

    第一次提交

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,20 @@
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "老林！";
+myHeading.textContent = "老覃！";
+
+let myImage = document.querySelector("img");
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/222.png") {
+    myImage.setAttribute("src", "images/Snipaste_2023-09-11_10-40-30.png");
+  }
+  else{
+    myImage.setAttribute("src", "images/222.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+  }
